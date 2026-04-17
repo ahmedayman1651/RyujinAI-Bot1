c6cc9693a45096c8a3e6bc893d627a4b61379ea1
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 18:00:08 +0200

    Delete ذكاء.js

--- ذكاء.js
+++
@@ -1,7 +0,0 @@
-const axios=require('axios');const إعداد=require('../config');
-module.exports={اسم:"ذكاء",async تنفيذ(sock,msg,args){
-const نص=args.join(" ");
-if(!نص)return sock.sendMessage(msg.key.remoteJid,{text:"اكتب سؤالك"});
-const res=await axios.post("https://api.openai.com/v1/chat/completions",{model:"gpt-4o-mini",messages:[{role:"user",content:نص}]},{headers:{Authorization:`Bearer ${إعداد.مفتاح_الذكاء}`}});
-sock.sendMessage(msg.key.remoteJid,{text:res.data.choices[0].message.content});
-}};
