9f084f659c81e5a4cad720cff880d90c6a3b94e0
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:57:30 +0200

    Delete قفل.js

--- قفل.js
+++
@@ -1,7 +0,0 @@
-module.exports={اسم:"قفل",تنفيذ(sock,msg,args){
-const id=msg.key.remoteJid;const وقت=parseInt(args[0]);
-if(!وقت)return sock.sendMessage(id,{text:"مدة؟"});
-global.قفل_الشات[id]=true;
-sock.sendMessage(id,{text:"🔒 تم القفل"});
-setTimeout(()=>{delete global.قفل_الشات[id];sock.sendMessage(id,{text:"🔓 فتح"});},وقت*60000);
-}};
