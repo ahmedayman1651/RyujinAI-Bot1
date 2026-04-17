f5de01f1c2ed7b4fd77be78fa6dc6f2943a609bd
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:59:28 +0200

    Delete طرد.js

--- طرد.js
+++
@@ -1,4 +0,0 @@
-module.exports={اسم:"طرد",async تنفيذ(sock,msg){
-const u=msg.message.extendedTextMessage?.contextInfo?.participant;
-if(u) await sock.groupParticipantsUpdate(msg.key.remoteJid,[u],"remove");
-}};
