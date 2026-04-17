814c3a666b8c883712c40f4a0039bc1bf046fff2
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:58:00 +0200

    Delete فيديو.js

--- فيديو.js
+++
@@ -1,4 +0,0 @@
-module.exports={اسم:"فيديو",تنفيذ(sock,msg,args){
-const q=args.join(" ")||"anime";
-sock.sendMessage(msg.key.remoteJid,{text:"🎬 https://www.youtube.com/results?search_query="+q});
-}};
