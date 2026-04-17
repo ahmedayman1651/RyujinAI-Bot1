bee2dce576c94f411e9c367fad1a378d9627851c
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:56:20 +0200

    Delete يومية.js

--- يومية.js
+++
@@ -1,5 +0,0 @@
-module.exports={اسم:"يومية",تنفيذ(sock,msg,{فلوس}){
-const id=msg.key.participant||msg.key.remoteJid;
-فلوس[id]=(فلوس[id]||0)+50;
-sock.sendMessage(msg.key.remoteJid,{text:"💰 +50"});
-}};
