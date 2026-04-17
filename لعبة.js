053abda1f2483ee83c1f10ce24b815f1740455db
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:57:09 +0200

    Delete لعبة.js

--- لعبة.js
+++
@@ -1,4 +0,0 @@
-module.exports={اسم:"لعبة",تنفيذ(sock,msg){
-const arr=["مين أقوى؟","قول اسم أنمي","تحدي"];
-sock.sendMessage(msg.key.remoteJid,{text:"🎮 "+arr[Math.floor(Math.random()*arr.length)]});
-}};
