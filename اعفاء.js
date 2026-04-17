073db860d9dd367dc42c75bfb7ea7d4d9d8d2ac1
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 18:00:40 +0200

    Delete اعفاء.js

--- اعفاء.js
+++
@@ -1,4 +0,0 @@
-module.exports={اسم:"اعفاء",async تنفيذ(sock,msg){
-const u=msg.message.extendedTextMessage?.contextInfo?.participant;
-if(u) await sock.groupParticipantsUpdate(msg.key.remoteJid,[u],"demote");
-}};
