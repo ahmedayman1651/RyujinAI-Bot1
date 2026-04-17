05b1c2baa90605a7d4a72c87d22f078d46477b50
ahmedayman1651 <ahmedmohmed800a@gmail.com>
2026-04-17 17:58:33 +0200

    Delete فتح.js

--- فتح.js
+++
@@ -1,7 +0,0 @@
-module.exports={اسم:"فتح",تنفيذ(sock,msg,args){
-const id=msg.key.remoteJid;const وقت=parseInt(args[0]);
-if(!وقت)return sock.sendMessage(id,{text:"مدة؟"});
-delete global.قفل_الشات[id];
-sock.sendMessage(id,{text:"🔓 تم الفتح"});
-setTimeout(()=>{global.قفل_الشات[id]=true;sock.sendMessage(id,{text:"🔒 قفل"});},وقت*60000);
-}};
